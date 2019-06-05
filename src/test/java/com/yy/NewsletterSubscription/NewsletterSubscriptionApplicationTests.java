dec0cd54754d08b07384050c3750625bdf8c5c7b
Yunukas <yunuss@gmail.com>
2019-06-05 08:20:12 -0700

    Initial commit

---
+++ src/test/java/com/yy/NewsletterSubscription/NewsletterSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yy.NewsletterSubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsletterSubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
